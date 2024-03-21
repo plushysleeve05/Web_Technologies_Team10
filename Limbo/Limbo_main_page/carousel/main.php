efc212b79c675ad966e7f004ee3ea502e6bcf3fb
Lois-Mills <122936557+Lois-Mills@users.noreply.github.com>
2024-03-21 21:11:52 +0000

    new file:   Final Team project/Limbo_main_page/blog/Community Page.html modified:   Limbo/Limbo_main_page/carousel/main.php new file:   Limbo/home/css/userstyle.css new file:   Limbo/home/userprofile.php new file:   images/.grey_background.jpg.icloud deleted:    images/grey_background.jpg modified:   view/help.html modified:   view/home.html

--- Limbo/Limbo_main_page/carousel/main.php
+++ Limbo/Limbo_main_page/carousel/main.php
@@ -166,6 +166,9 @@
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="../../index.php">Home</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../../../Limbo/home/userprofile.php">User Profile</a>
+          </li>
       </div>
     </div>
   </nav>
